0e8752d7908987590e7e85fd8fe6484c01286238
Ramon Mendez <mendezpoli86@gmail.com>
2026-02-14 14:19:03 +0000

    Fix several segmentation fail. Add basic water phisics

--- compile.bat
+++ compile.bat
@@ -5,7 +5,7 @@ echo ========================================
 echo.
 
 echo [1/2] Compilando main.zc a C...
-..\zc.com build main.zc --emit-c
+..\zc.com transpile main.zc
 
 
 echo.
@@ -14,7 +14,7 @@ echo [2/2] Compilando out.c con GCC...
 REM Crear la carpeta bin si no existe para evitar errores
 if not exist bin mkdir bin
 
-gcc out.c -o ./bin/zencraft.exe -L. -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc -g out.c -o ./bin/zencraft.exe -L. -lraylib -lopengl32 -lgdi32 -lwinmm
 
 REM Comprobar si GCC fallo
 if %errorlevel% neq 0 (
