4fe96dbe6995d77ec1c83c03ba3032cb51a9b475
Ramon Mendez <mendezpoli86@gmail.com>
2026-05-04 11:43:23 +0100

    fix: Update to work with Zen C v0.4.4

--- compile.bat
+++ compile.bat
@@ -3,23 +3,23 @@ echo ========================================
 echo       Construyendo ZenCraft...
 echo ========================================
 echo.
-REM Borrando out.c si existe
-if exist out.c del out.c
+REM Borrando main.c si existe
+if exist main.c del main.c
 
 
 echo [1/2] Compilando main.zc a C...
 ..\zc.com transpile ./src/main.zc
 
-echo [1.5/2] Limpiando rutas absolutas en out.c...
-powershell -Command "(gc out.c) -replace '/C/', 'C:/' | Out-File -encoding ASCII out.c"
+echo [1.5/2] Limpiando rutas absolutas en main.c...
+powershell -Command "(gc main.c) -replace '/C/', 'C:/' | Out-File -encoding ASCII main.c"
 
 echo.
-echo [2/2] Compilando out.c con GCC...
+echo [2/2] Compilando main.c con GCC...
 
 REM Crear la carpeta bin si no existe para evitar errores
 if not exist bin mkdir bin
 
-gcc -g out.c -o ./bin/zencraft.exe -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm
+gcc -g main.c -o ./bin/zencraft.exe -L./lib -lraylib -lopengl32 -lgdi32 -lwinmm
 
 echo [2.5/2] Copiando dependencias a bin...
 copy /Y .\lib\libraylib.dll .\bin\
